654e7ccaad58bdcce4f7c04df2bcc78d3c913d3d
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-10 00:28:51 +0500

    wylsacom_clean_code

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/Hudayberdyyev/crawler/models"
 	"github.com/Hudayberdyyev/crawler/repository"
 	"github.com/Hudayberdyyev/crawler/repository/postgres"
 	"github.com/Hudayberdyyev/crawler/repository/storage"
+	"github.com/Hudayberdyyev/crawler/source/wylsacom"
 	"github.com/jackc/pgx"
 	"github.com/minio/minio-go/v7"
 	"log"
@@ -13,6 +15,7 @@ import (
 
 const (
 	ParsingInterval = 1 // on seconds
+	Wylsacom = 3
 )
 
 func main() {
@@ -68,13 +71,10 @@ func RunParser(repo *repository.Repository, second int) {
 	ticker := time.NewTicker(time.Duration(second) * time.Second)
 
 	for _ = range ticker.C{
-		// ============================================================
-		// TurkmenPortal.ParseTurkmenPortal(repo, models.News{
-		//			CatID:  0,
-		//			AuthID: TurkmenPortalID,
-		//			Image:  "",
-		//		})
-		// ============================================================
-		fmt.Println("everything up to date !!!")
+		wylsacom.StartParser(repo, models.News{
+			CatID: 0,
+			AuthID: Wylsacom,
+			Image: "",
+		})
 	}
 }
---
+++ source/wylsacom/wylsacom.go
@@ -0,0 +1,11 @@
+package wylsacom
+
+import (
+	"fmt"
+	"github.com/Hudayberdyyev/crawler/models"
+	"github.com/Hudayberdyyev/crawler/repository"
+)
+
+func StartParser(repo *repository.Repository, newsInfo models.News) {
+	fmt.Println("everything up to date")
+}
