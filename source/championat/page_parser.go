04477bc1f3ca87b53d4b170b4ccae093a24de50d
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-13 00:50:24 +0500

    getLink

--- source/championat/page_parser.go
+++ source/championat/page_parser.go
@@ -47,12 +47,13 @@ func NewsPageParser(repo *repository.Repository, URL string, latestLink string, 
 
 	fmt.Println(URL)
 	sel := doc.Find("div.page-content > div.page-main > div.news._all > div.news-items").Children()
+	var postDate string
 	for i := range sel.Nodes {
 		s := sel.Eq(i)
 		// ====================================================================
 		// if tag is date then get date
 		// ====================================================================
-		var postDate string
+
 		if s.Nodes[0].Attr[0].Val == "news-items__head" {
 			splitPostDate := strings.Split(strings.Trim(s.Text(), " \n\t\r"), " ")
 			if cap(splitPostDate) != 3 {
@@ -66,132 +67,132 @@ func NewsPageParser(repo *repository.Repository, URL string, latestLink string, 
 			}
 
 			postDate = postDay + "." + postMonth + "." + postYear
-			fmt.Println(postDate)
-			continue
-		}
-
-		continue
-
-		// ====================================================================
-		// image
-		// ====================================================================
-		imageLink, ok := s.Find("img").Attr("src")
-		if !ok {
-			log.Printf("No news images\n")
-			continue
-		}
-		newsInfo.Image = "https://rozetked.me" + imageLink
-
-		// ====================================================================
-		// link
-		// ====================================================================
-		link, ok := s.Find("div.post_new-title > a").Attr("href")
-		if !ok {
-			log.Printf("No news link\n")
 			continue
 		}
 
 		// ====================================================================
-		// publishDate
+		// if tag is article then get info
 		// ====================================================================
-		metaSel := s.Find("div.post_new-meta > div.post_new-meta-author").Children().Eq(0)
-		publishDateStr := strings.Trim(metaSel.Text(), " \n\t\r")
+		if s.Nodes[0].Attr[0].Val == "news-item" {
 
-		splitDate := strings.Split(publishDateStr, " ")
+			// ====================================================================
+			// image
+			// ====================================================================
+			newsInfo.Image = "https://championat.com/static/i/svg/logo.svg"
 
-		if cap(splitDate) == 2 {
-			splitPublishTime := splitDate[0]
-			splitPublishDate := splitDate[1]
-			splitPublishTime = splitPublishTime + ":00"
-			publishDateStr = splitPublishTime + " " + splitPublishDate + " +03:00"
-		} else {
-			splitPublishTime := splitDate[0]
-			timeType := splitDate[1]
+			// ====================================================================
+			// link
+			// ====================================================================
+			link, ok := s.Find("div.news-item__content > a").Eq(0).Attr("href")
+			if !ok {
+				log.Printf("No news link\n")
+				continue
+			}
 
-			var durationType string
-			if strings.Contains(timeType, "час") {
-				durationType = "h"
+			link = "https://championat.com" + link
+
+			// ====================================================================
+			// publishDate
+			// ====================================================================
+			metaSel := s.Find("div.post_new-meta > div.post_new-meta-author").Children().Eq(0)
+			publishDateStr := strings.Trim(metaSel.Text(), " \n\t\r")
+
+			splitDate := strings.Split(publishDateStr, " ")
+
+			if cap(splitDate) == 2 {
+				splitPublishTime := splitDate[0]
+				splitPublishDate := splitDate[1]
+				splitPublishTime = splitPublishTime + ":00"
+				publishDateStr = splitPublishTime + " " + splitPublishDate + " +03:00"
 			} else {
-				if strings.Contains(timeType, "мин") {
-					durationType = "m"
+				splitPublishTime := splitDate[0]
+				timeType := splitDate[1]
+
+				var durationType string
+				if strings.Contains(timeType, "час") {
+					durationType = "h"
 				} else {
-					if strings.Contains(timeType, "сек") {
-						durationType = "s"
+					if strings.Contains(timeType, "мин") {
+						durationType = "m"
+					} else {
+						if strings.Contains(timeType, "сек") {
+							durationType = "s"
+						}
 					}
 				}
-			}
-			postDuration, err := time.ParseDuration("-" + splitPublishTime + durationType)
-			if err != nil {
-				log.Printf("error with parse duration %s: %v\n", splitPublishTime, err)
-			}
-			publishDateStr = time.Now().Add(postDuration).Format(layoutDateTime)
-		}
-
-		publishDate, err := time.Parse(layoutDateTime, publishDateStr)
-		if err != nil {
-			log.Printf("error with parse date: %v\n", err)
-		}
-		newsInfo.PublishDate = publishDate
-
-		// ====================================================================
-		// checking a new article
-		// ====================================================================
-		if link == latestLink {
-			fmt.Println("everything up to date !")
-			return http.StatusNotModified
-		}
-
-		// ====================================================================
-		//	article to db
-		// ====================================================================
-		newsId, e := repo.CreateNews(newsInfo)
-		if e != nil {
-			log.Printf("Error with create news: %v\n", e)
-			continue
-		}
-
-		s.Find("div.post_new__main_box_bottom-tags > a").Each(func(i int, tagSelection *goquery.Selection) {
-			tagText := strings.Trim(tagSelection.Text(), " \n\t\r")
-
-			// ====================================================================
-			//	get TagID
-			// ====================================================================
-			tagId, err := repo.Database.GetTagIdByName(tagText)
-			if err != nil {
-				log.Printf("error with get tag id by name: %v\n", err)
-			}
-
-			// ====================================================================
-			//	if there is no such tags then create a new and get ID
-			// ====================================================================
-			if tagId == 0 {
-				tagId, err = repo.Database.CreateTags(tagText, ru)
+				postDuration, err := time.ParseDuration("-" + splitPublishTime + durationType)
 				if err != nil {
-					log.Printf("error with create tag by name: %v\n", err)
+					log.Printf("error with parse duration %s: %v\n", splitPublishTime, err)
+				}
+				publishDateStr = time.Now().Add(postDuration).Format(layoutDateTime)
+			}
+
+			publishDate, err := time.Parse(layoutDateTime, publishDateStr)
+			if err != nil {
+				log.Printf("error with parse date: %v\n", err)
+			}
+			newsInfo.PublishDate = publishDate
+
+			// ====================================================================
+			// checking a new article
+			// ====================================================================
+			if link == latestLink {
+				fmt.Println("everything up to date !")
+				return http.StatusNotModified
+			}
+
+			// ====================================================================
+			//	article to db
+			// ====================================================================
+			newsId, e := repo.CreateNews(newsInfo)
+			if e != nil {
+				log.Printf("Error with create news: %v\n", e)
+				continue
+			}
+
+			s.Find("div.post_new__main_box_bottom-tags > a").Each(func(i int, tagSelection *goquery.Selection) {
+				tagText := strings.Trim(tagSelection.Text(), " \n\t\r")
+
+				// ====================================================================
+				//	get TagID
+				// ====================================================================
+				tagId, err := repo.Database.GetTagIdByName(tagText)
+				if err != nil {
+					log.Printf("error with get tag id by name: %v\n", err)
+				}
+
+				// ====================================================================
+				//	if there is no such tags then create a new and get ID
+				// ====================================================================
+				if tagId == 0 {
+					tagId, err = repo.Database.CreateTags(tagText, ru)
+					if err != nil {
+						log.Printf("error with create tag by name: %v\n", err)
+						return
+					}
+				}
+
+				_, err = repo.Database.CreateNewsTags(newsId, tagId)
+				if err != nil {
+					log.Printf("error with create news tags: %v\n", err)
 					return
 				}
+			})
+
+			// ====================================================================
+			// image article to storage
+			// ====================================================================
+			uploadErr := repo.UploadImage(context.Background(), "news", newsInfo.Image, strconv.Itoa(newsId))
+			if uploadErr != nil {
+				log.Printf("error with upload image: %v\n", uploadErr)
 			}
 
-			_, err = repo.Database.CreateNewsTags(newsId, tagId)
-			if err != nil {
-				log.Printf("error with create news tags: %v\n", err)
-				return
-			}
-		})
-
-		// ====================================================================
-		// image article to storage
-		// ====================================================================
-		uploadErr := repo.UploadImage(context.Background(), "news", newsInfo.Image, strconv.Itoa(newsId))
-		if uploadErr != nil {
-			log.Printf("error with upload image: %v\n", uploadErr)
+			// ====================================================================
+			// add ids and links articles to slices
+			// ====================================================================
+			ids = append(ids, newsId)
+			result = append(result, link)
 		}
-
-		// ====================================================================
-		// add ids and links articles to slices
-		// ====================================================================
-		ids = append(ids, newsId)
-		result = append(result, link)
 	}
 
 	// ====================================================================
