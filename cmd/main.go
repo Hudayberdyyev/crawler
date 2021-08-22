ffe2de2ca837523bd76f77d208e9f7ba12511d3d
WIN-DBOTML7B1JH\User <hudayberdyyev.ahmet@gmail.com>
2021-08-22 16:03:03 +0500

    fixMain_ParsingInterval

--- cmd/main.go
+++ cmd/main.go
@@ -14,28 +14,28 @@ import (
 	"github.com/jackc/pgx"
 	"github.com/minio/minio-go/v7"
 	"log"
-	"os"
+	"time"
 )
 
 const (
 	ParsingInterval = 1 // on seconds
 	TurkmenPortalID = 1
-	Rozetked = 2
-	Wylsacom = 3
-	Championat = 4
-	IXBT = 5
+	Rozetked        = 2
+	Wylsacom        = 3
+	Championat      = 4
+	IXBT            = 5
 )
 
 func main() {
 	fmt.Println("crawler is starting ... ")
 
 	db, err := initDB(postgres.Config{
-		Host: "db",
+		Host:     "db",
 		Username: "postgres",
 		Password: "qwerty",
-		Port: 5432,
-		DBName: "postgres",
-		SSLMode: "disable",
+		Port:     5432,
+		DBName:   "postgres",
+		SSLMode:  "disable",
 	})
 
 	if err != nil {
@@ -45,10 +45,10 @@ func main() {
 	defer db.Close()
 
 	minioClient, err := initMinio(storage.Config{
-		Endpoint: "storage"+":9000",
-		AccessKeyId: "AHMET",
+		Endpoint:       "storage" + ":9000",
+		AccessKeyId:    "AHMET",
 		SecretAccesKey: "Ah25101996!",
-		UseSSL: false,
+		UseSSL:         false,
 	})
 
 	repository := repository.NewRepository(db, minioClient)
@@ -57,10 +57,10 @@ func main() {
 		log.Fatalf("Error with init minio: %v\n", err)
 	}
 
-	RunParser(repository)
+	RunParser(repository, ParsingInterval)
 }
 
-func initDB(config postgres.Config)  (*pgx.Conn, error){
+func initDB(config postgres.Config) (*pgx.Conn, error) {
 
 	db, err := postgres.NewPostgresDB(config)
 
@@ -75,53 +75,46 @@ func initMinio(config storage.Config) (*minio.Client, error) {
 	return storage.NewMinio(config)
 }
 
-func RunParser(repo *repository.Repository) {
-		src := os.Getenv("SOURCE")
-		fmt.Println(src)
-		if src == "turkmenportal" {
-			fmt.Println("Crawling [turkmenportal]")
-			TurkmenPortal.ParseTurkmenPortal(repo, models.News{
-				CatID:  0,
-				AuthID: TurkmenPortalID,
-				Image:  "",
-			})
-		}
+func RunParser(repo *repository.Repository, second int) {
+	ticker := time.NewTicker(time.Duration(second) * time.Second)
+
+	for _ = range ticker.C {
 		// ============================================================
-		if src == "rozetked" {
-			fmt.Println("Crawling [rozetked]")
-			rozetked.StartParser(repo, models.News{
-				CatID:  0,
-				AuthID: Rozetked,
-				Image:  "",
-			})
-		}
+		fmt.Println("Crawling [turkmenportal]")
+		TurkmenPortal.ParseTurkmenPortal(repo, models.News{
+			CatID:  0,
+			AuthID: TurkmenPortalID,
+			Image:  "",
+		})
 		// ============================================================
-		if src == "wylsacom" {
-			fmt.Println("Crawling [wylsacom]")
-			wylsacom.StartParser(repo, models.News{
-				CatID: 0,
-				AuthID: Wylsacom,
-				Image: "",
-			})
-		}
+		fmt.Println("Crawling [rozetked]")
+		rozetked.StartParser(repo, models.News{
+			CatID:  0,
+			AuthID: Rozetked,
+			Image:  "",
+		})
 		// ============================================================
-		if src == "championat" {
-			fmt.Println("Crawling [championat]")
-			championat.StartParser(repo, models.News{
-				CatID:  0,
-				AuthID: Championat,
-				Image:  "",
-			})
-		}
+		fmt.Println("Crawling [wylsacom]")
+		wylsacom.StartParser(repo, models.News{
+			CatID:  0,
+			AuthID: Wylsacom,
+			Image:  "",
+		})
 		// ============================================================
-		if src == "ixbt" {
-			fmt.Println("Crawling [ixbt]")
-			ixbt.StartParser(repo, models.News{
-				CatID:  0,
-				AuthID: IXBT,
-				Image:  "",
-			})
-		}
+		fmt.Println("Crawling [championat]")
+		championat.StartParser(repo, models.News{
+			CatID:  0,
+			AuthID: Championat,
+			Image:  "",
+		})
+		// ============================================================
+		fmt.Println("Crawling [ixbt]")
+		ixbt.StartParser(repo, models.News{
+			CatID:  0,
+			AuthID: IXBT,
+			Image:  "",
+		})
 		// ============================================================
 		fmt.Println("everything up to date !!!")
+	}
 }
